51fd6028a9b3d2e2e951a71cc164b596e5a6e23c
dipanjan4u <36997214+dipanjan4u@users.noreply.github.com>
2018-03-17 13:07:52 +0000

    1st Unit test case added for case sensitive single word search

--- src/test/java/com/cts/search/SearchServiceApplicationTests.java
+++ src/test/java/com/cts/search/SearchServiceApplicationTests.java
@@ -1,16 +1,34 @@
 package com.cts.search;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.cts.search.controller.SearchController;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(SearchController.class)
 public class SearchServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private MockMvc searchTest;
 
+	@Test
+	public void singleWordSearchCaseSensitive() throws Exception {
+		searchTest
+				.perform(get("/search/files").param("recursivesearch", "Y").param("casesensitive", "Y")
+						.param("searchwords", "rahul").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.TOTAL", is(3)));
+
+	}
 }
